7adbc4864ebc631ace42f6bc3c1e53c749a7f21d
Fabian Deutsch <fabiand@fedoraproject.org>
2016-02-26 20:58:49 +0100

    Add proxy support
    Signed-off-by: Fabian Deutsch <fabiand@redhat.com>

--- container/ngn-builder/build-ngn.sh
+++ container/ngn-builder/build-ngn.sh
@@ -10,8 +10,11 @@ curl -O -L http://mirror.centos.org/centos/7/os/x86_64/images/boot.iso
 
 sed -i -e "/CURLOPTS/ d" automation/build-artifacts.sh
 
-libvirtd &
+libvirtd --listen &
 
+LMC="livemedia-creator --ram 2048 --vcpus 4"
+[[ -n "$http_proxy" ]] && LMC="$LMC --proxy $http_proxy"
+export LMC
 bash -ex automation/build-artifacts.sh || { tail -n40 *.log ; exit 1 ; }
 
 make check
