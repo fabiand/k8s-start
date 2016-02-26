8c7bab5f10d25b5c6bd373c5852017bf6b4d9ce9
Fabian Deutsch <fabiand@fedoraproject.org>
2016-02-26 20:58:46 +0100

    Progress

--- container/ngn-builder/build-ngn.sh
+++ container/ngn-builder/build-ngn.sh
@@ -6,13 +6,14 @@ git clone http://gerrit.ovirt.org/ovirt-node-ng.git
 
 cd ovirt-node-ng
 
-ln /boot.iso boot.iso
-touch boot.iso
+curl -O -L http://mirror.centos.org/centos/7/os/x86_64/images/boot.iso
 
 sed -i -e "/CURLOPTS/ d" automation/build-artifacts.sh
 
 libvirtd &
 
 bash -ex automation/build-artifacts.sh || { tail -n40 *.log ; exit 1 ; }
+
+make check
 
 find
