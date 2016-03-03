25f82700bc4c2f9bc95687eede8c5c329e8e83f2
Fabian Deutsch <fabiand@fedoraproject.org>
2016-03-03 16:33:37 +0100

    Assign bridge to VM
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- container/compute/start.sh
+++ container/compute/start.sh
@@ -4,6 +4,13 @@ set -xe
 
 libvirtd --listen &
 
+symlink_default_nic_to_well_known_name() {
+  local DEFAULT_NIC=$(ip route show to 0/0 | cut -d" " -f 5)
+  ip link show meth0 || ip link add name meth0 link $DEFAULT_NIC type macvlan
+  ip link set meth0 up
+  # macvatp does not work in container
+}
+
 sleep 3
 
 if [[ -n "$LIBVIRT_DOMAIN_URL" ]]; then
